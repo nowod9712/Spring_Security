4351e79847ff11d4d9d32dc041f3f9f3c74967cd
nowod <ldowon1223@naver.com>
2024-03-18 17:31:23 +0900

    member아이디 admin 접근제한

---
+++ src/main/java/org/zerock/controller/CommonController.java
@@ -0,0 +1,22 @@
+package org.zerock.controller;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class CommonController {
+
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+		
+		log.info("access Denied : " + auth);
+		
+		model.addAttribute("msg", "Access Denied");
+	}
+	
+}
