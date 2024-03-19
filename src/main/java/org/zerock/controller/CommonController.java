c70117ae604c872f85d9947d6ef094141e5fef1d
nowod <ldowon1223@naver.com>
2024-03-19 10:43:41 +0900

    커스텀 로그인 페이지 구현

--- src/main/java/org/zerock/controller/CommonController.java
+++ src/main/java/org/zerock/controller/CommonController.java
@@ -19,4 +19,20 @@ public class CommonController {
 		model.addAttribute("msg", "Access Denied");
 	}
 	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		
+		log.info("errer : " + error);
+		log.info("logout : " + logout);
+		
+		if(error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+
+		if(logout != null) {
+			model.addAttribute("logout", "Logout!");
+		}
+		
+	}
+	
 }
